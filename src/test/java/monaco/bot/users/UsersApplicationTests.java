5b39ad9efb3f3a3ff537cb3ee92402e55c6ada3c
Andrei <andrei.khaustov.nextlevel@gmail.com>
2025-02-22 17:07:26 +0300

    Initial commit

---
+++ src/test/java/monaco/bot/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package monaco.bot.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
